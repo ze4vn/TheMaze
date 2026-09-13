f2785be0e7aacf9f86c43c0fd449e7841cc299c3
Zev <nixonuxi44@gmail.com>
2026-09-13 06:04:59 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,26 +2,16 @@ import { Game } from './Game.js';
 
 const menu = document.getElementById('mainMenu');
 const btnStart = document.getElementById('btnStartGame');
-const btnQuit = document.getElementById('btnQuit');
-const gameOverlay = document.getElementById('gameOverlay');
 
 let game = null;
 
 function startGame() {
     menu.classList.add('hidden');
-    gameOverlay.classList.add('active');
     if (!game) {
         game = new Game();
         game.init();
         window.__game = game;
-    } else {
-        game.restartLevels();
     }
 }
 
 btnStart.addEventListener('click', startGame);
-btnQuit.addEventListener('click', () => {
-    if (confirm('Quit game?')) {
-        window.close();
-    }
-});
